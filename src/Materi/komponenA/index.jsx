5ccee4496a4c883b3811d642e782c2f7f83e482f
wayne1098 <wayne10pnd@gmail.com>
2022-06-28 04:48:04 +0700

    tugas lifecycle edit7

---
+++ src/Materi/komponenA/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ClassComponent from "./pembahasan/ClassComponent";
+import FunctionalComponent from "./pembahasan/FunctionalComponent";
+ 
+export default class komponenA extends React.Component {
+ 
+	render() {
+		return (
+			<div>
+			<ClassComponent nama="wawan" />
+			<FunctionalComponent nama="gunawan" />
+			</div>
+		)
+	} 
+}
